f13f4e5a2ca8a16308fc12eb187c48062f677874
Ranger <mnipk1243@gmail.com>
2025-11-27 11:09:26 +0500

    steps

--- main.cpp
+++ main.cpp
@@ -18,6 +18,13 @@ map<char, int> GetFrequency(string str){
     
     return mp;
 }
-
+//1. frequency calculation
+//2. unique characters numbers
+//3. choose numbers of bits
+//4. create binary tree for each character, with its frequency as the root value
+//5. keep on taking two minimum frequency roots, add them to create a new root, add those minimum roots as subtrees of the new root
+//6. repeat until one root is achieved
+//7. calculate bit, if left 0, else right 1
+//8. 
 
 
