a4ab94b04cc8197c53e28a96c33f76387ea28462
Ranger <mnipk1243@gmail.com>
2025-11-30 21:55:01 +0500

    added header file for huffman encoder

---
+++ hfm.cpp
@@ -0,0 +1,1 @@
+#include "hfm.h"
---
+++ hfm.h
@@ -0,0 +1,21 @@
+#ifndef HUFFMANENCODER_H
+#define HUFFMANENCODER_H
+#include <iostream>
+using namespace std;
+
+class HuffmanEncoder{
+    private:
+    //binary tree list
+    //file text
+    //encoded text
+    //frequency calculator
+    //extract string from file
+    //binary tree function
+    //
+    public:
+    //encode
+    //decode
+    //
+
+};
+#endif HUFFMANENCODER_H
